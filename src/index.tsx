62abde5f800518791ac5fe30ba710e951f4614ec
idunnsta <idunnsta@ntnu.no>
2022-10-17 12:32:26 +0200

    🎨  byttet til scss formatt i  koden

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import styles from "./index.module.scss";
+import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 
-
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <ChakraProvider>
