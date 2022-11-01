8ce89760d2bc1ea6323561aea85bcd46973a1722
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-01 15:27:42 +0000

    Removed test-route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -7,7 +7,6 @@ import NotFoundPage from './pages/NotFound';
 import LogIn from './pages/auth/LogInPage';
 import RegisterUser from './pages/auth/RegisterPage';
 import RequireAuthPage from './pages/auth/RequireAuthPage';
-import Foo from './pages/Foo';
 
 const RoutesRoute = () => {
   return (
@@ -16,7 +15,6 @@ const RoutesRoute = () => {
         <Route path="*" element={<NotFoundPage />} />
         <Route path={'/login'} element={<LogIn />} />
         <Route path={'/register'} element={<RegisterUser />} />
-        <Route path="/foo" element={<Foo />} />
         <Route element={<RequireAuthPage />}>
           <Route path={'/'} element={<PatientsPage />} />
           <Route path={'patient/:patientId'} element={<PatientPage />} />
