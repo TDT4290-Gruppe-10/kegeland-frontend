b5f24f849e51efffd83c19036cb439ced2533dc6
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-01 15:27:33 +0000

    Added global configuration for momentjs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
+import moment from 'moment';
+import momentTimezone from 'moment-timezone';
+import 'moment/locale/en-gb';
+import './styles/reset.css';
 
 import App from './App';
+
+// Global configuration for moment
+moment.locale('en-gb');
+momentTimezone.tz.setDefault('Europe/Oslo');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
