1c94682da8027ac7ea3820c2747c8bd6cacb5511
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-02 16:10:31 +0000

    Refactored routes-structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Box } from '@chakra-ui/react';
+import { useRoutes } from 'react-router-dom';
 
 import { store, persistor } from './state/store';
-import RoutesRoute from './Routes';
 import withSilentRefresh from './hoc/withSilentRefresh';
+import routes from './routes';
 
 const Wrapper = withSilentRefresh(Box, {
   backgroundColor: 'gray.100',
@@ -13,12 +14,11 @@ const Wrapper = withSilentRefresh(Box, {
 });
 
 function App() {
+  const renderRoutes = useRoutes(routes);
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Wrapper>
-          <RoutesRoute />
-        </Wrapper>
+        <Wrapper>{renderRoutes}</Wrapper>
       </PersistGate>
     </Provider>
   );
