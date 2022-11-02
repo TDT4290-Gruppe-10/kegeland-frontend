ee861f62c91d01ab2a84f206c92fa4cbe3b109de
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-02 16:15:19 +0000

    Refactored auth-state

--- src/state/ducks/auth/auth.helpers.ts
+++ src/state/ducks/auth/auth.helpers.ts
@@ -1,4 +1,7 @@
+import { PayloadAction } from '@reduxjs/toolkit';
 import { every, values } from 'lodash';
+
+import { AuthState, LoginResponse } from './auth.interface';
 
 export enum Token {
   ACCESS_TOKEN = 'access_token',
@@ -20,4 +23,20 @@ export const clearSignedInState = () => {
     authUser: undefined,
     userDetails: undefined,
   };
+};
+
+export const signInReducer = (
+  state: AuthState,
+  action: PayloadAction<LoginResponse>,
+) => {
+  const { id, email } = action.payload;
+  state.isSignedIn = true;
+  state.authUser = { id, email };
+  state.userDetails = action.payload.details;
+};
+
+export const signOutReducer = (state: AuthState) => {
+  state.isSignedIn = false;
+  state.authUser = undefined;
+  state.userDetails = undefined;
 };
