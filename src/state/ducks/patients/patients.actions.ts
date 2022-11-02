276c0fe0cc798800aad99b9bdafc128854ea78b3
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-02 16:14:56 +0000

    Refactored patients-state

--- src/state/ducks/patients/patients.actions.ts
+++ src/state/ducks/patients/patients.actions.ts
@@ -2,42 +2,22 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { apiCaller } from '../../../utils/apiCaller';
 
-export const getAllPatients = createAsyncThunk(
-  'users/get-all-users',
+import { Patient } from './patients.interface';
+
+export const fetchPatientById = createAsyncThunk(
+  'patients/fetchPatientById',
+  async (id: string) =>
+    apiCaller<Patient>({
+      url: `users/${id}`,
+      method: 'GET',
+    }),
+);
+
+export const fetchPatients = createAsyncThunk(
+  'patients/fetchPatients',
   async () =>
-    apiCaller<any>({ url: 'users/get-all-patients', method: 'GET' }).then(
-      async (res) => {
-        return res;
-      },
-    ),
-);
-
-export const getAllPatientSessions = createAsyncThunk(
-  'sensors/sessions',
-  async (uid: string) =>
-    apiCaller<any>({ url: `sessions?userId${uid}`, method: 'GET' }).then(
-      async (res) => {
-        return res;
-      },
-    ),
-);
-
-export const getPatientExercise = createAsyncThunk(
-  'sensors/sessions',
-  async (sessionId: string) =>
-    apiCaller<any>({ url: `session/${sessionId}`, method: 'GET' }).then(
-      async (res) => {
-        return res;
-      },
-    ),
-);
-
-export const getPatientOverview = createAsyncThunk(
-  'users/overview',
-  async (userId: string) =>
-    apiCaller<any>({ url: `users/overview/${userId}`, method: 'GET' }).then(
-      async (res) => {
-        return res;
-      },
-    ),
+    apiCaller<Patient[]>({
+      url: 'users/patients',
+      method: 'GET',
+    }),
 );
