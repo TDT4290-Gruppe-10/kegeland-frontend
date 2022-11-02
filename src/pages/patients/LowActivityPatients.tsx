c4fab40d79e87785852594850988670cc366d6c9
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-02 16:09:31 +0000

    Removed old files

--- src/pages/patients/LowActivityPatients.tsx
+++
@@ -1,5 +0,0 @@
-const LowActivityPatients = () => {
-  return <div>Low activity patients</div>;
-};
-
-export default LowActivityPatients;
--- src/utils/Things.tsx
+++
@@ -1,9 +0,0 @@
-export type menuItemsType = { [index: string]: string };
-
-export const getTextFromkey = (menuItems: menuItemsType, key: string) => {
-  if (key in menuItems) {
-    return menuItems[key];
-  }
-
-  return Object.values(menuItems)[0];
-};
