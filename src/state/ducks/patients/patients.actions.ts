6fdbb0378929bce387c26c96d5ecb90b7a7e59aa
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-03 17:02:17 +0100

    Resolve merge conflict

--- src/state/ducks/patients/patients.actions.ts
+++ src/state/ducks/patients/patients.actions.ts
@@ -3,6 +3,11 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { apiCaller } from '../../../utils/apiCaller';
 
 import { Patient } from './patients.interface';
+export const getAllPatients = createAsyncThunk('users/patients', async () =>
+  apiCaller<any>({ url: 'users/patients', method: 'GET' }).then(async (res) => {
+    return res;
+  }),
+);
 
 export const fetchPatientById = createAsyncThunk(
   'patients/fetchPatientById',
