84086420117c7ed13a4fb881d5fcdaaaf9d7b290
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-07 13:40:26 +0100

    Added jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'react',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['node_modules/?!(static-container)'],
+  cacheDirectory: '.jest/cache',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  clearMocks: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!**/node_modules/**'],
+};
